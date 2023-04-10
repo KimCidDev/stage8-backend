ad03d7b81515ce936fa0fa7f122b77c6ade7f91e
Kim <cid.kim@gmail.com>
2023-04-10 13:09:57 -0600

    get request params example. Xis calabresa

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+app.get('/sabores/:id', (request, response) => {
+  response.send(
+    `Você acessou os sabores de Xis do Porto. Xis escolhido pelo usuário: ${request.params.id}`
+  )
+})
 const PORT = 3333
 
 app.listen(PORT, () => {
