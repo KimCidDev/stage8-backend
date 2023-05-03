b1a740b54b897c626f9d9d191aa67e0fb8fdaf58
Kim <cid.kim@gmail.com>
2023-05-03 06:27:28 -0600

    linking controller to notes.routes.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const { Router } = require('express');
 const routes = Router();
 
 const usersRouter = require('./users.routes');
+const routesRouter = require('./users.routes.js');
 
 routes.use('/users', usersRouter);
+routes.use('/notes', routesRouter);
 
 module.exports = routes;
